b655849dd19ae2a5cda57ef81d65f17358331452
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2021-01-22 19:54:39 +0300

    Add project files.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    public class MyDictionary<TK, TV>
+    {
+        TK[] TKey; TV[] TValue;
+
+        public MyDictionary()
+        {
+            TKey = new TK[0];
+            TValue = new TV[0];
+        }
+
+        public void Add(TK key, TV value)
+        {
+            TK[] tempKey = TKey;
+            TV[] tempValue = TValue;
+
+            TKey = new TK[TKey.Length + 1];
+            TValue = new TV[TValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                TKey[i] = tempKey[i];
+                TValue[i] = tempValue[i];
+            }
+
+            TKey[TKey.Length - 1] = key;
+            TValue[TValue.Length - 1] = value;
+        }
+
+        public int Lenght {
+            get { return TKey.Length; }
+        }
+
+        public TK[] GetKeys {
+            get { return TKey; }
+        }
+
+        public TV[] GetValues
+        {
+            get { return TValue; }
+        }
+
+
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Oğuzhan");
+            myDictionary.Add(2, "Aydinay");
+            myDictionary.Add(3, "Everything is alright!");
+
+            foreach (var item in myDictionary.GetKeys)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in myDictionary.GetValues)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
